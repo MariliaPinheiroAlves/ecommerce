cc6f16f12e7299326b37589206bd0d388eee8d9a
João Marcos <56234707+joaomcsferreira@users.noreply.github.com>
2024-02-19 21:20:09 -0300

    feat: adicionado acesso publico a pasta images

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,28 @@
-import express, { Response, Express } from "express";
-import cors from "cors";
+import express, { Response, Express } from "express"
+import cors from "cors"
 
-import { CustomCors } from "./middlewares/customCors";
-import { routes } from "./routes/routes";
+import { CustomCors } from "./middlewares/customCors"
+import { routes } from "./routes/routes"
 
-import dotenv from "dotenv";
+import dotenv from "dotenv"
 
-dotenv.config();
+dotenv.config()
 
 const main = () => {
-  const app: Express = express();
-  const PORT = process.env.PORT;
+    const app: Express = express()
+    const PORT = process.env.PORT
 
-  app.use(express.json());
-  app.use(cors());
-  app.use(CustomCors);
+    app.use(express.json())
+    app.use(cors())
+    app.use(CustomCors)
+    app.use("/images", express.static("./images"))
 
-  app.listen(PORT, () =>
-    console.log(`Server running at http://localhost:${PORT}/`)
-  );
+    app.listen(PORT, () =>
+        console.log(`Server running at http://localhost:${PORT}/`)
+    )
 
-  app.get("/", (_, response: Response) => response.send({ version: "1.0.0" }));
-  routes(app);
-};
+    app.get("/", (_, response: Response) => response.send({ version: "1.0.0" }))
+    routes(app)
+}
 
-main();
+main()
